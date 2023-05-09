e733f203332fa56a04f9e4c4ff6a62793ec2832c
root <root@DESKTOP-31HM7L6.localdomain>
2023-05-09 14:13:33 -0400

    setup server.js

--- server.js
+++ server.js
@@ -102,3 +102,47 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+
+import {rps, rpsls} from "./lib/rpsls.js"
+i
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// check for 200 ok
+app.get('/app/', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+})
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+})
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.query.shot)));
+});
+app.get('/app/rpsls/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+});
+app.post('/app/rps/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.body.shot)));
+});
+app.post('/app/rpsls/play', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+});
+app.get('/app/rps/play/:shot', (req, res) => {
+	res.status(200).send(JSON.stringify(rps(req.params.shot)));
+});
+app.get('/app/rpsls/play/:shot', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+});
+
+
+// calling a nonexistent endpoint
+app.get('*', (req, res) => {
+	res.status(404).send('404 NOT FOUND');
+});
